d66619ab59ddaedfc7ea12e154be440e9f98d686
soleepoli <soleepoli@gmail.com>
2025-09-02 16:25:31 +0900

    커밋 테스트

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,7 +11,7 @@ import benefitRoutes from './routes/benefit';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(helmet());
